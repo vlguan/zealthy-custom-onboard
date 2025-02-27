44fb76a93b7679555394108a4d7e60e05daa267f
Vince Guan <vinceguan@gmail.com>
2025-02-27 01:06:27 -0800

    forgot one api endpoint

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -19,7 +19,7 @@ const DataPage: React.FC = () => {
     useEffect(() => {
         const loadData = async () => {
             try{
-                const response = await axios.get(`${process.env.BACKEND_URL}/api/display`);
+                const response = await axios.get(`https://${process.env.NEXT_PUBLIC_APP_API_URL}/api/display`);
                 const data = JSON.parse(response.data.success)
                 setData(data)
             } catch (error){
